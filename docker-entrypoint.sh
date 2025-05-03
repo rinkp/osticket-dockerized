003c41c6cb3368f46f946f3dd585c1a563ce78f4
Rink <rinkp@users.noreply.github.com>
2025-05-03 20:25:31 +0000

    feat(docker): migrate to alpine

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,31 +1,48 @@
 #!/bin/sh
 
-# If we already have a config file, simply drop setup folder
-if [ -f "/var/www/html/include/ost-config.php" ]; then
-    rm -rf /var/www/html/setup
-fi
+set -uo pipefail
 
-# Create database etc. if it does not exist yet, but make sure to use our own configuration
-if [ -d "/var/www/html/setup/" -a ! -f "/var/www/html/include/ost-config.php" ]; then
-    cp -rf /config/* /var/www/html/include
-    chmod +w /var/www/html/include/ost-config.php
-    cd /var/www/html/setup
-    php ./envbasedinstall.php
-    if [ -f "/var/www/html/setup/success" ]; then
-        cd ..
-        rm -rf /var/www/html/setup
-        chmod 0644 /var/www/html/include/ost-config.php
-        chown -R www-data:www-data /var/www/attachments/
-    else
+# Verify that the attachments directory is a volume (to prevent dataloss)
+if [ ! -z "$OST_PLUGINS_STORAGEFS_PATH" ]; then
+    cat /etc/mtab | grep " $OST_PLUGINS_STORAGEFS_PATH " > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "\$OST_PLUGINS_STORAGEFS_PATH=\"$OST_PLUGINS_STORAGEFS_PATH\", but \"$OST_PLUGINS_STORAGEFS_PATH\" is not a volume"
         exit 1
     fi
 fi
 
+# If we already have a config file, simply drop setup folder
+if [ -f "/var/www/localhost/htdocs/include/ost-config.php" ]; then
+    rm -rf /var/www/localhost/htdocs/setup
+fi
+
+# Create database etc. if it does not exist yet, but make sure to use our own configuration
+if [ -d "/var/www/localhost/htdocs/setup/" -a ! -f "/var/www/localhost/htdocs/include/ost-config.php" ]; then
+    cp -rf /config/* /var/www/localhost/htdocs/include
+    chmod +w /var/www/localhost/htdocs/include/ost-config.php
+    cd /var/www/localhost/htdocs/setup
+    php ./envbasedinstall.php
+    if [ -f "/var/www/localhost/htdocs/setup/success" ]; then
+        cd ..
+        rm -rf /var/www/localhost/htdocs/setup
+        chmod 0644 /var/www/localhost/htdocs/include/ost-config.php
+        chown -R apache:apache $OST_PLUGINS_STORAGEFS_PATH
+    else
+        # Undo setting the configuration
+        rm -f /var/www/localhost/htdocs/include/ost-config.php
+        exit 1
+    fi
+fi
+
+if [ ! -z "$OST_HELPDESK_URL" ]; then
+    echo -n "\$OST_HELPDESK_URL=\"$OST_HELPDESK_URL\", setting Apache servername: "
+    echo "ServerName ${OST_HELPDESK_URL}" | sed -E "s/https?:\/\///g" | tee /etc/apache2/conf.d/servername.conf
+fi
 
 # Prepare scheduled tasks using cron
-env >> /etc/environment
-cron
-chmod 644 /etc/cron.d/osticketcron
+env | grep -E '^(OST|PHP).*' >> /etc/environment
+crond
+chmod 0644 /etc/cron.d/osticketcron
 
 #launch the application
-apache2-foreground
+httpd -DFOREGROUND
--- setup/envbasedinstall.php
+++ setup/envbasedinstall.php
@@ -40,12 +40,84 @@ if ($status) {
     $staff = Staff::lookup(1);
     $staff->forcePasswdRest();
     $staff->save();
-    echo "Succesfully run installation. You can now sign in with username " . ADMIN_EMAIL . " and password " . getenv("OST_ADMIN_PASSWD");
-    touch("../setup/success");
+    echo "Succesfully run installation. You can now sign in with username " . ADMIN_EMAIL . " and password " . getenv("OST_ADMIN_PASSWD") . PHP_EOL;
 } elseif (!empty($status->prefix)) { // If we previously ran the installation, we can silently exit
     var_dump($installer->errors);
     exit(1);
 }
 
+/**
+ * Enable changing osticket configuration items
+ */
+$config = new OsticketConfig();
+
+if (strlen(getenv("OST_HELPDESK_URL") ?: '') > 0) {
+    echo '"OST_HELPDESK_URL=' . getenv("OST_HELPDESK_URL") . '", configuring helpdesk URL' . PHP_EOL;
+    $config->set('helpdesk_url', getenv("OST_HELPDESK_URL"));
+}
+
+if (strlen(getenv("OST_HELPDESK_ONLINE") ?: '') > 0) {
+    echo '"OST_HELPDESK_ONLINE=' . getenv("OST_HELPDESK_ONLINE") . '", configuring online status' . PHP_EOL;
+    $isonline = 1 - (strtolower(getenv("OST_HELPDESK_ONLINE")) === "false");
+    echo '-> set isonline to ' . $isonline . PHP_EOL;
+    $config->set('isonline', $isonline);
+}
+
+/**
+ * Set storage attachment backend if environment is configured
+ */
+if (strlen(getenv("OST_PLUGINS_STORAGEFS_PATH") ?: '') > 0) {
+    echo '"OST_PLUGINS_STORAGEFS_PATH=' . getenv("OST_PLUGINS_STORAGEFS_PATH") . '", configuring storage' . PHP_EOL;
+
+    $storageFsPlugin = PluginManager::getInstance(getenv("OST_PLUGINS_STORAGEFS_PLUGIN"));
+    if ($storageFsPlugin === null) {
+        echo '-> Installing "' . getenv("OST_PLUGINS_STORAGEFS_PLUGIN") . '"' . PHP_EOL;
+        $pluginmanager = new PluginManager();
+        $pluginmanager->install(getenv("OST_PLUGINS_STORAGEFS_PLUGIN"));
+        PluginManager::clearCache();
+        $storageFsPlugin = PluginManager::getInstance(getenv("OST_PLUGINS_STORAGEFS_PLUGIN"));
+    }
+
+    if (!$storageFsPlugin->isActive()) {
+        $errors = [];
+        $storageFsPlugin->update([
+            'isactive' => true,
+            'notes' => 'Automatically enabled by osticket-dockerized because "OST_PLUGINS_STORAGEFS_PATH" was set',
+        ], $errors);
+        if (count($errors) > 0) {
+            print_r($errors);
+            echo '-> Could not enable "' . getenv("OST_PLUGINS_STORAGEFS_PLUGIN") . '", exiting...' . PHP_EOL;
+            exit(1);
+        } else {
+            echo '-> Enabled "' . getenv("OST_PLUGINS_STORAGEFS_PLUGIN") . '"' . PHP_EOL;
+        }
+    }
+
+    if ($storageFsPlugin->getNumInstances() === 0) {
+        $errors = [];
+        $storageFsPlugin->addInstance([
+            'name' => 'Attachments-fs ' . getenv("OST_PLUGINS_STORAGEFS_PATH"),
+            'uploadpath' => getenv("OST_PLUGINS_STORAGEFS_PATH"),
+            'isactive' => true,
+        ], $errors);
+        if (count($errors) > 0) {
+            print_r($errors);
+            echo '-> Could not configure instance of "' . getenv("OST_PLUGINS_STORAGEFS_PLUGIN") . '", exiting...' . PHP_EOL;
+            exit(1);
+        } else {
+            echo '-> Configured instance of "' . getenv("OST_PLUGINS_STORAGEFS_PLUGIN") . '"' . PHP_EOL;
+            PluginManager::clearCache();
+        }
+
+        // Make this new plugin the default attachment storage
+        $storageFsPlugin->getActiveInstances()[0]->bootstrap();
+        $storageChars = FileStorageBackend::allRegistered();
+        $fileStorageBackendChar = array_search('FilesystemStorage', $storageChars);
+        $config->set('default_storage_bk', $fileStorageBackendChar);
+    }
+}
+
+// If we get here, setup was succesfull
+touch("../setup/success");
 
 ?>
